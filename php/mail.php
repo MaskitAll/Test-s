0ccb5fcbf1d296f400d5151325eb268df542b71b
MaskitAll <maskit-zzz@list.ru>
2021-02-04 14:12:27 +0300

    bug fixed, linked mail-func

--- php/mail.php
+++ php/mail.php
@@ -1,27 +1,48 @@
 <?php
-// echo("Hello world!");
 
+// $name = $_POST['name'];
+// $mail = $_POST['mail'];
+// $theme = $_POST['theme'];
+// $message = $_POST['message'];
 
-$name = $_POST['name'];
-$mail = $_POST['mail'];
-$theme = $_POST['theme'];
-$message = $_POST['message'];
+// $name = htmlspecialchars($name);
+// $message = htmlspecialchars($message);
 
-// echo($name." ".$theme."\n");
-echo("Hello world!");
+// $name = urldecode($name);
+// $message = urldecode($message);
 
-$name = htmlspecialchars($name);
-$message = htmlspecialchars($message);
+// $name = trim($name);
+// $message = trim($message);
 
-$name = urldecode($name);
-$message = urldecode($message);
+// if (mail("fking94@mail.ru", "Theme ".$theme."\n", "ФИО: ".$name."\nMessage: ".$message)) {
+//      echo "\nсообщение успешно отправлено";
+// } else {
+//     echo "\nпри отправке сообщения возникли ошибки";
+// }
 
-$name = trim($name);
-$message = trim($message);
+	
+// Подключаем библиотеку PHPMailer
+use PHPMailer\PHPMailer\PHPMailer;
+require 'D:\phpmailer\src\PHPMailer.php';
+ 
+// Создаем письмо
+$mail = new PHPMailer();
+$mail->setFrom('maskit-zzz@list.ru', 'Иван Иванов'); // от кого (email и имя)
+$mail->addAddress('fking94@mail.ru', 'Вася Петров'); // кому (email и имя)
+$mail->Subject = 'Тест';                             // тема письма
 
-if (!mail("maskit-zzz@list.ru", "Theme", "ФИО: ".$name."Message: ".$message)) {
-     echo "\nсообщение успешно отправлено";
+// html текст письма
+$mail->CharSet = 'UTF-8';
+$mail->isHTML(true);
+
+$mail->msgHTML("<html><body>
+                <h1>Здравствуйте!</h1>
+                <p>Это тестовое письмо.</p>
+                </html></body>");
+// Отправляем
+if ($mail->send()) {
+  echo 'Письмо отправлено!';
 } else {
-    echo "\nпри отправке сообщения возникли ошибки";
-}
+  echo 'Ошибка: ' . $mail->ErrorInfo;
+}  
 ?>
