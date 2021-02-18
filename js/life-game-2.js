d3af580ce2dbc2c7b97f92e71856cd5b3d799596
MaskitAll <maskit-zzz@list.ru>
2021-02-18 04:27:31 +0300

    bug fixed

--- js/life-game-2.js
+++ js/life-game-2.js
@@ -1,4 +1,13 @@
 'use strict';
+const   canvas  = document.getElementById("sea-battle-canvas"),
+        ctx     = canvas.getContext('2d'); 
+           
+        canvas.width = Math.floor(canvas.offsetWidth);
+        canvas.height =  Math.floor(canvas.offsetHeight);
+        
+        ctx.fillStyle = "#ccc";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 
 var start = document.querySelector("#lifeStart"),
 	pause = document.querySelector("#lifePause"),
@@ -12,15 +21,81 @@ var size_range = document.querySelector("#lifeSize");
 var lifeTimer;
 var lifeSpeed = 500;
 
-
-class battleShipMap extends Map 
-{
-    constructor(id){
-        super(id);
-        // this.width = 10;
-        // this.height = 10;
-    }
+// переводит позицию мыши на экране в позицию на канвасе
+function windowToCanvas(x, y, indent = 5) {
+    var bbox = canvas.getBoundingClientRect();
+    
+    return {
+        // позиция мыши - (позиция канваса(0.0) + отступ)
+        x: x - (bbox.left + indent),
+        y: y - (bbox.top + indent)
+    };
 }
 
-// var m1 = new battleShipMap("sea-battle-canvas" );
-// console.log(m1);
+canvas.addEventListener('mousedown', function (e) {
+
+    var loc = windowToCanvas(e.clientX, e.clientY, m.indent);
+    var x = loc.x;
+    var y = loc.y;
+    
+    x = Math.floor(x / (canvas.width - m.indent * 2) * m.width);
+    y = Math.floor(y / (canvas.height - m.indent * 2) * m.height);
+
+    // изменить в зависимости от логики
+    if(m.cellMap[x][y].value < m.colorArray.length - 1){ m.cellMap[x][y].value++;}
+    else {m.cellMap[x][y].value = 0;}
+
+    drawMap(m);
+});
+
+
+function drawBorder(m, color, x, y){
+    ctx.fillStyle = color;
+
+    ctx.fillRect(   m.indent + x * ((canvas.width - m.indent * 2) / m.width) - 2,
+                    m.indent + y * ((canvas.height - m.indent * 2) / m.height) - 2,
+                    (canvas.width - m.indent * 2)/ m.width + 2,
+                    (canvas.height - m.indent * 2) / m.height + 2
+    );
+};
+
+function drawCell(m, x, y){
+    ctx.fillStyle = m.colorArray[m.cellMap[x][y].value];
+
+    ctx.fillRect(   m.indent + x * ((canvas.width - m.indent * 2) / m.width),
+                    m.indent + y * ((canvas.height - m.indent * 2) / m.height),
+                    (canvas.width - m.indent * 2)/ m.width - 2,
+                    (canvas.height - m.indent * 2) / m.height - 2
+    );
+};
+
+
+// отрисовывает карту
+function drawMap(m){
+    for (var i = 0; i < m.cellMap.length; ++i){
+        for (var j = 0; j < m.cellMap[i].length; ++j){
+            drawCell(m, i, j);
+            // console.log("m");
+        }
+    }
+};
+
+
+// количество клеток определяется масштабом (10px)
+var Ms = 20;
+var w = Math.floor(canvas.width / Ms); 
+var h = Math.floor(canvas.height / Ms); 
+
+var m = new Map(6, 6, 5);
+    m.fillMap();
+
+    m.cellMap[0][0].value = 1;
+    m.cellMap[5][5].value = 1;
+    drawBorder(m, "#0f0", 3, 3);
+    // m.cellMap[1][0].value = 1;
+    // m.cellMap[1][1].value = 1;
+    // m.swipeCell(m.cellMap[0][0], m.cellMap[1][0]);
+    drawMap(m);
+
+
+
--- js/map.js
+++ js/map.js
@@ -1,4 +1,47 @@
 
+/* Cell */
+class Cell{
+    constructor(x = 0, y = 0, value = 0) {
+        this.x = x;
+        this.y = y;
+
+        this.value = value;
+    }
+
+    setValue(newValue){
+        this.value = newValue;
+    }
+};
+
+/* Map */
+class Map{
+    constructor(width, height, indent){
+        this.width = width;
+        this.height = height;
+        this.indent = indent;
+        
+        this.cellMap = createArr(this.width, this.height);
+        this.colorArray = ["#fff", "#f00", "#0f0", "#00f", "#ccc", "#000"];
+        // this.colorArray = {0: "#fff", 1: "#f00", 2: "#0f0", 3: "#00f", 4: "#ccc", 5: "#000"};
+    }
+
+    // заполняет карту пустыми ячейками
+    fillMap(){
+        for(var i = 0; i < this.width; ++i){
+            for(var j = 0; j < this.height; ++j){
+                this.cellMap[i][j] = new Cell(i, j, 0);
+            }
+        }
+    }
+    // меняет местами значения ячеек
+    swipeCell(cell_1, cell_2){
+        var cell = new Cell();
+        cell.value = cell_2.value;
+        cell_2.value = cell_1.value;
+        cell_1.value = cell.value;
+    }
+}
+
 
 /* Всякие функции поддержки*/
 
@@ -17,117 +60,3 @@ function createArr(width, height){
 	}
 	return arr;
 }
-
-// Work with canvas
-function map(id){
-    const   canvas  = document.getElementById(id),
-            ctx     = canvas.getContext('2d');
-
-            canvas.width = Math.floor(canvas.offsetWidth);
-            canvas.height =  Math.floor(canvas.offsetHeight);
-        
-            ctx.fillStyle = "#ccc";
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-        
-    
-    /* Cell */
-    class Cell{
-        constructor(x = 0, y = 0, value = 0) {
-            this.x = x;
-            this.y = y;
-
-            this.value = value;
-        }
-
-        setValue(newValue){
-            this.value = newValue;
-        }
-        
-        drawCell(color = "#ddd", width = 10, height = 10, indent = 5){
-            ctx.fillStyle = color;
-            
-            ctx.fillRect(   indent + this.x * ((canvas.width - indent * 2) / width),
-                            indent + this.y * ((canvas.height - indent * 2) / height),
-                            (canvas.width - indent * 2)/ width - 2,
-                            (canvas.height - indent * 2) / height - 2
-            );
-        }
-    };
-
-    /* Map */
-    class Map{
-        constructor(width, height, indent){
-            this.width = width;
-            this.height = height;
-            this.indent = indent;
-            
-            this.cellMap = createArr(this.width, this.height);
-            this.colorArray = {0: "#fff", 1: "#f00", 2: "#0f0", 3: "#00f", 4: "#ccc", 5: "#000"}
-        }
-
-        // заполняет карту пустыми ячейками
-        fillMap(){
-            for(var i = 0; i < this.width; ++i){
-                for(var j = 0; j < this.height; ++j){
-                    this.cellMap[i][j] = new Cell(i, j, 0);
-                }
-            }
-        }
-
-        // отрисовывает карту
-        drawMap(){
-            for (var i = 0; i < this.cellMap.length; ++i){
-                for (var j = 0; j < this.cellMap[i].length; ++j){
-                    this.cellMap[i][j].drawCell(this.colorArray[this.cellMap[i][j].value], this.width, this.height, this.indent);
-                }
-            }
-        }
-    }
-
-    // переводит позицию мыши на экране в позицию на канвасе
-    function windowToCanvas(x, y, indent = 5) {
-        var bbox = canvas.getBoundingClientRect();
-
-        return {
-            // позиция мыши - (позиция канваса(0.0) + отступ)
-            x: x - (bbox.left + indent),
-            y: y - (bbox.top + indent)
-        };
-    }
-
-    canvas.addEventListener('mousedown', function (e) {
-
-            var loc = windowToCanvas(e.clientX, e.clientY, m.indent);
-            var x = loc.x;
-            var y = loc.y;
-
-            x = Math.floor(x / (canvas.width - m.indent * 2) * m.width);
-            y = Math.floor(y / (canvas.height - m.indent * 2) * m.height);
-
-            // изменить в зависимости от логики
-            if(m.cellMap[x][y].value < 5){ m.cellMap[x][y].value++;}
-            else {m.cellMap[x][y].value = 0;}
-
-            m.drawMap();
-        });
-
-
-    // количество клеток определяется масштабом (10px)
-    var Ms = 20;
-    var w = Math.floor(canvas.width / Ms); 
-    var h = Math.floor(canvas.height / Ms); 
-
-    var m = new Map(w, h, 5);
-    m.fillMap();
-    m.drawMap();
-}
-
-
-
-
-
-
-
-// map("example");
-map("sea-battle-canvas");
-
