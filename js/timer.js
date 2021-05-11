40d2e294daabf6e6d704397fa3aa1454c4df0069
MaskitAll <maskit-zzz@list.ru>
2021-05-11 11:45:39 +0300

    .

--- js/timer.js
+++ js/timer.js
@@ -72,7 +72,7 @@ var isSes = false;								//это сессия или отдых
 				Ding(400, 50, 10);					// DING!!!
 			}
 			tic();
-			timerid = setTimeout(timer, 1000, true);
+			timerid = setTimeout(timer, 100, true);
 		}
 		else{stop();}
 	}
